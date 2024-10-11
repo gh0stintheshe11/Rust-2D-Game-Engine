80075191ab4ec6886d28e449946fbd56abba33b0
gh0stintheshe11 <langs.971104@gmail.com>
2024-10-11 07:14:17 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ fn main() {
     let native_options: NativeOptions = NativeOptions {
         vsync: true, // Enable vertical sync
         hardware_acceleration: HardwareAcceleration::Preferred, // Use default hardware acceleration
-        run_and_return: false, // Don't run the app and return the window
-        centered: true, // Center the window
         ..Default::default() // Use default values for other fields
     };
 
