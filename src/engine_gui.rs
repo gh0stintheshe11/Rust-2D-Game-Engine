5bdce7056ef9902317e14c986e2b0a59b1a4e191
gh0stintheshe11 <langs.971104@gmail.com>
2024-10-11 07:11:26 -0400

    update

--- src/engine_gui.rs
+++ src/engine_gui.rs
@@ -1,23 +1,23 @@
-use eframe::egui;
 use crate::project_file_manager::FileManagement;
+use eframe::egui;
 
 #[derive(Default)]
 pub struct EngineGui {
-    show_new_project_popup: bool,   // Track if the pop-up should be shown
+    show_new_project_popup: bool,  // Track if the pop-up should be shown
     show_open_project_popup: bool, // Track if the pop-up should be shown
     load_project: bool,            // Track if the project should be loaded
-    project_name: String,           // Store the project name input
-    project_path: String,           // Store the project path input
-    pub terminal_output: String,         // Store the terminal output
+    project_name: String,          // Store the project name input
+    project_path: String,          // Store the project path input
+    pub terminal_output: String,   // Store the terminal output
 }
 
 impl eframe::App for EngineGui {
-    fn update(&mut self, window: &egui::Context, _frame: &mut eframe::Frame) {
-        // Calculate window width and height once at the start
-        let window_width = window.screen_rect().width();
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Calculate ctx width and height once at the start
+        let ctx_width = ctx.screen_rect().width();
 
         // Display the main menu bar
-        self.show_main_menu_bar(window, window_width);
+        self.show_main_menu_bar(ctx, ctx_width);
 
         // Display the pop-up for creating a new project
         if self.show_new_project_popup {
@@ -27,22 +27,22 @@ impl eframe::App for EngineGui {
             egui::Window::new("Create New Project")
                 .resizable(false)
                 .collapsible(false)
+                .fade_in(true)
                 .frame(
-                    egui::Frame::window(&egui::Style::default())
-                        .shadow(egui::epaint::Shadow {
-                            offset: egui::Vec2::new(0.0, 0.0), // Adjust the shadow offset
-                            blur: 0.0, // Adjust the shadow blur
-                            spread: 2.0, // Adjust the shadow spread
-                            color: egui::Color32::DARK_GRAY, // Add the shadow colors
-                        })
+                    egui::Frame::window(&egui::Style::default()).shadow(egui::epaint::Shadow {
+                        offset: egui::Vec2::new(0.0, 0.0), // Adjust the shadow offset
+                        blur: 0.0,                         // Adjust the shadow blur
+                        spread: 2.0,                       // Adjust the shadow spread
+                        color: egui::Color32::DARK_GRAY,   // Add the shadow colors
+                    }),
                 )
-                .show(window, |ui| {
+                .show(ctx, |ui| {
                     ui.label("Project Name:");
                     ui.text_edit_singleline(&mut self.project_name);
-        
+
                     ui.label("Project Path:");
                     ui.text_edit_singleline(&mut self.project_path);
-        
+
                     // Start horizontal layout for buttons
                     ui.horizontal(|ui| {
                         if ui.button("Create").clicked() {
@@ -54,9 +54,10 @@ impl eframe::App for EngineGui {
 
                                 // Call FileManagement to create the project, passing self
                                 FileManagement::create_project(&project_name, &project_path, self);
-                                
+
                                 // Format the project path to be a valid path
-                                self.project_path = format!("{}/{}", self.project_path, self.project_name);
+                                self.project_path =
+                                    format!("{}/{}", self.project_path, self.project_name);
                                 // If the project is created successfully, set the load_project to true
                                 self.load_project = true;
                                 self.show_new_project_popup = false; // Close the popup after creation
@@ -78,16 +79,16 @@ impl eframe::App for EngineGui {
             egui::Window::new("Open Project")
                 .resizable(false)
                 .collapsible(false)
+                .fade_in(true)
                 .frame(
-                    egui::Frame::window(&egui::Style::default())
-                        .shadow(egui::epaint::Shadow {
-                            offset: egui::Vec2::new(0.0, 0.0), // Adjust the shadow offset
-                            blur: 0.0, // Adjust the shadow blur
-                            spread: 2.0, // Adjust the shadow spread
-                            color: egui::Color32::DARK_GRAY, // Add the shadow colors
-                        })
+                    egui::Frame::window(&egui::Style::default()).shadow(egui::epaint::Shadow {
+                        offset: egui::Vec2::new(0.0, 0.0), // Adjust the shadow offset
+                        blur: 0.0,                         // Adjust the shadow blur
+                        spread: 2.0,                       // Adjust the shadow spread
+                        color: egui::Color32::DARK_GRAY,   // Add the shadow colors
+                    }),
                 )
-                .show(window, |ui| {
+                .show(ctx, |ui| {
                     ui.label("Project Path:");
                     ui.text_edit_singleline(&mut self.project_path);
 
@@ -100,14 +101,21 @@ impl eframe::App for EngineGui {
                                     // Change the load_project to true
                                     self.load_project = true;
                                     // Get the project name and path from the project.json file
-                                    let project_metadata = FileManagement::read_project_metadata(&self.project_path);
-                                    self.project_name = project_metadata.project_name; 
-                                    self.project_path = project_metadata.project_path; 
+                                    let project_metadata =
+                                        FileManagement::read_project_metadata(&self.project_path);
+                                    self.project_name = project_metadata.project_name;
+                                    self.project_path = project_metadata.project_path;
                                     // print out
-                                    self.print_to_terminal(&format!("Project name: {} in {} loaded", self.project_name, self.project_path));
+                                    self.print_to_terminal(&format!(
+                                        "Project name: {} in {} loaded",
+                                        self.project_name, self.project_path
+                                    ));
                                     self.show_open_project_popup = false; // Close the popup after opening
                                 } else {
-                                    self.print_to_terminal(&format!("Invalid project path: {}", self.project_path));
+                                    self.print_to_terminal(&format!(
+                                        "Invalid project path: {}",
+                                        self.project_path
+                                    ));
                                 }
                             } else {
                                 self.print_to_terminal("Project path is required.");
@@ -118,24 +126,21 @@ impl eframe::App for EngineGui {
                         }
                     });
                 });
-
         }
 
         // Display the three-panel layout
-        self.show_three_panel_layout(window, window_width);
+        self.show_three_panel_layout(ctx, ctx_width);
     }
 }
 
 impl EngineGui {
-
     // Main menu bar at the top
-    fn show_main_menu_bar(&mut self, window: &egui::Context, window_width: f32) {
-
+    fn show_main_menu_bar(&mut self, ctx: &egui::Context, ctx_width: f32) {
         egui::TopBottomPanel::top("main_menu_bar")
             .resizable(false)
             .min_height(20.0)
-            .show(window, |ui| {
-                ui.set_width(window_width);
+            .show(ctx, |ui| {
+                ui.set_width(ctx_width);
 
                 // Horizontal layout for File and Edit menus
                 ui.horizontal(|ui| {
@@ -165,18 +170,16 @@ impl EngineGui {
     }
 
     // Three-panel layout with left, right, and central panels
-    fn show_three_panel_layout(&mut self, window: &egui::Context, window_width: f32) {
-
-        let item_spacing = window.style().spacing.item_spacing; // Get the vertical spacing between elements
+    fn show_three_panel_layout(&mut self, ctx: &egui::Context, ctx_width: f32) {
+        let item_spacing = ctx.style().spacing.item_spacing; // Get the vertical spacing between elements
 
         // Left panel (split into top and bottom)
         egui::SidePanel::left("asset")
             .resizable(true)
-            .default_width(window_width * 0.15)
-            .width_range((window_width * 0.15)..=(window_width * 0.3))
-            .show(window, |ui| {
-
-                let secondary_panel_height = ui.available_height()*0.5;
+            .default_width(ctx_width * 0.15)
+            .width_range((ctx_width * 0.15)..=(ctx_width * 0.3))
+            .show(ctx, |ui| {
+                let secondary_panel_height = ui.available_height() * 0.5;
 
                 // Top section
                 egui::TopBottomPanel::top("entity_inspector")
@@ -187,7 +190,7 @@ impl EngineGui {
                         ui.heading("Entity Inspector");
                         ui.label("Inspect and modify the attributes of the entity");
                     });
-               
+
                 // Bottom section
                 let entity_folder_path = format!("{}/entities", self.project_path);
                 egui::TopBottomPanel::bottom("entity")
@@ -196,37 +199,44 @@ impl EngineGui {
                     .show_separator_line(false)
                     .frame(egui::Frame::none().inner_margin(egui::Margin::same(0.0)))
                     .show_inside(ui, |ui| {
-
                         let heading_response = ui.heading("Entities");
                         let heading_height = heading_response.rect.height(); // Get the height of the heading
 
                         // Wrapping the entire list of buttons in the scroll area
                         if self.load_project {
                             egui::ScrollArea::vertical()
-                                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded)
-                                .max_height(secondary_panel_height - heading_height - 3.0*item_spacing.y)
+                                .scroll_bar_visibility(
+                                    egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded,
+                                )
+                                .max_height(
+                                    secondary_panel_height - heading_height - 3.0 * item_spacing.y,
+                                )
                                 .auto_shrink([false; 2]) // Prevent shrinking when there is less content
                                 .show(ui, |ui| {
-                                    let files = FileManagement::list_files_in_folder(&entity_folder_path, self);
+                                    let files = FileManagement::list_files_in_folder(
+                                        &entity_folder_path,
+                                        self,
+                                    );
                                     for file in files {
                                         if ui.button(&file).clicked() {
-                                            self.print_to_terminal(&format!("Clicked on file: {}", file));
+                                            self.print_to_terminal(&format!(
+                                                "Clicked on file: {}",
+                                                file
+                                            ));
                                         }
                                     }
                                 });
                         }
                     });
-
             });
 
         // Right panel (split into top and bottom)
         egui::SidePanel::right("script")
             .resizable(true)
-            .default_width(window_width*0.15)
-            .width_range((window_width*0.15)..=(window_width*0.3))
-            .show(window, |ui| {
-
-                let secondary_panel_height = ui.available_height()*0.5;
+            .default_width(ctx_width * 0.15)
+            .width_range((ctx_width * 0.15)..=(ctx_width * 0.3))
+            .show(ctx, |ui| {
+                let secondary_panel_height = ui.available_height() * 0.5;
 
                 // Top section
                 egui::TopBottomPanel::top("script_inspector")
@@ -237,7 +247,7 @@ impl EngineGui {
                         ui.heading("Script Inspector");
                         ui.label("Inspect and modify the attributes of the script");
                     });
-               
+
                 // Bottom section
                 let script_folder_path = format!("{}/scripts", self.project_path);
                 egui::TopBottomPanel::bottom("script")
@@ -246,53 +256,62 @@ impl EngineGui {
                     .show_separator_line(false)
                     .frame(egui::Frame::none().inner_margin(egui::Margin::same(0.0)))
                     .show_inside(ui, |ui| {
-
                         let heading_response = ui.heading("Scripts");
                         let heading_height = heading_response.rect.height(); // Get the height of the heading
 
                         // Wrapping the entire list of buttons in the scroll area
                         if self.load_project {
                             egui::ScrollArea::vertical()
-                                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded)
-                                .max_height(secondary_panel_height - heading_height - 3.0*item_spacing.y)
+                                .scroll_bar_visibility(
+                                    egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded,
+                                )
+                                .max_height(
+                                    secondary_panel_height - heading_height - 3.0 * item_spacing.y,
+                                )
                                 .auto_shrink([false; 2]) // Prevent shrinking when there is less content
                                 .show(ui, |ui| {
-                                    let files = FileManagement::list_files_in_folder(&script_folder_path, self);
+                                    let files = FileManagement::list_files_in_folder(
+                                        &script_folder_path,
+                                        self,
+                                    );
                                     for file in files {
                                         if ui.button(&file).clicked() {
-                                            self.print_to_terminal(&format!("Clicked on file: {}", file));
+                                            self.print_to_terminal(&format!(
+                                                "Clicked on file: {}",
+                                                file
+                                            ));
                                         }
                                     }
                                 });
                         }
                     });
-
             });
 
         // Central panel
-        egui::CentralPanel::default().show(window, |ui| {
+        egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Scene");
             ui.label("Scene Viewer");
         });
 
         // Bottom panel for terminal
         egui::TopBottomPanel::bottom("terminal")
-            .resizable(true)    
-            .min_height((window.screen_rect().height()-20.0)*0.2)
-            .max_height((window.screen_rect().height()-20.0)*0.5)
-            .show(window, |ui| {
+            .resizable(true)
+            .min_height((ctx.screen_rect().height() - 20.0) * 0.2)
+            .max_height((ctx.screen_rect().height() - 20.0) * 0.5)
+            .show(ctx, |ui| {
+                ui.heading("Terminal");
 
-            ui.heading("Terminal");
-            
-            // Wrap the terminal output in a scroll area
-            egui::ScrollArea::vertical()
-                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded)
-                .auto_shrink([false; 2])
-                .max_height(ui.available_height() - item_spacing.y)
-                .stick_to_bottom(true) // Ensure the scroll area always scrolls to the bottom
-                .show(ui, |ui| {
-                ui.label(&self.terminal_output); // Display the terminal output
-                });
+                // Wrap the terminal output in a scroll area
+                egui::ScrollArea::vertical()
+                    .scroll_bar_visibility(
+                        egui::scroll_area::ScrollBarVisibility::VisibleWhenNeeded,
+                    )
+                    .auto_shrink([false; 2])
+                    .max_height(ui.available_height() - item_spacing.y)
+                    .stick_to_bottom(true) // Ensure the scroll area always scrolls to the bottom
+                    .show(ui, |ui| {
+                        ui.label(&self.terminal_output); // Display the terminal output
+                    });
             });
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 mod engine_gui;
 mod project_file_manager;
-use eframe::NativeOptions;
+use eframe::*;
 mod physics_engine;
 
 fn main() {
     // Set the native options for the window
     let native_options: NativeOptions = NativeOptions {
         vsync: true, // Enable vertical sync
+        hardware_acceleration: HardwareAcceleration::Preferred, // Use default hardware acceleration
+        run_and_return: false, // Don't run the app and return the window
+        centered: true, // Center the window
         ..Default::default() // Use default values for other fields
     };
 
     // Run the app with the window title and options
-    eframe::run_native(
+    let _ = eframe::run_native(
         "Rust 2D Game Engine",
         native_options,
-        Box::new(|_cc| Ok(Box::new(engine_gui::EngineGui::default()))), // Wrap in Ok
+        Box::new(|_cc| Ok(Box::new(engine_gui::EngineGui::default()))),
     );
 }
